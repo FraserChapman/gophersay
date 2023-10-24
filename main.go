5a79a4e1a2e972132a35ada52facafa0ce75cb23
Fraser Chapman <fraser.chapman@gmail.com>
2023-10-24 16:49:34 +0100

    Added newline after gopher

--- main.go
+++ main.go
@@ -20,7 +20,8 @@ const (
   ]            | | |            |
   ]             ~ ~             |
   |                            |
-   |                           |`
+   |                           |
+`
 )
 
 func maxLen(text ...string) int {
